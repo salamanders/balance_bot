75fa44b2ac07a1b6b111c8b2fad54151f7aed592
Benjamin Hill <salamanders@users.noreply.github.com>
2026-01-25 16:30:32 -0800

    Enable I2C interface and install MPU6050 library
    Added commands to enable I2C interface and install MPU6050 library.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+# Enable I2C interface if you haven't yet
+sudo raspi-config nonint do_i2c 0
+
+
 sudo apt-get update
 sudo apt-get install python3-smbus i2c-tools
 # Get the Picon Zero library
 wget http://4tronix.co.uk/piconz.sh -O piconz.sh
 bash piconz.sh
+
+# Install the MPU6050 library
+pip3 install mpu6050-raspberrypi
